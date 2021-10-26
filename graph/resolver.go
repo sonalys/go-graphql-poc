b33a9fb5b3e4b3c413199e47d9b7cead3f6788f0
Alysson Ribeiro <alraicon@gmail.com>
2021-10-26 14:07:33 -0300

    initial commit

---
+++ graph/resolver.go
@@ -0,0 +1,12 @@
+//go:generate go run github.com/99designs/gqlgen
+package graph
+
+import "github.com/sonalys/go-graphql-poc/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
